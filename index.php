4bbd17055a02b5a920c706838e668dbee520d2c6
MirandaHack1 <miranda3791167@gmail.com>
2024-05-22 16:04:23 -0500

    operadores arimetricos

--- index.php
+++ index.php
@@ -4,4 +4,7 @@ echo "PRIMERA CLASE CONCADENAR";
 // DECLARACION DE VARIABLES
 $nombre = "Juan";
 $apellido = "Perez";
+// CONCADENAR
 echo "Hola " . $nombre ." " . $apellido;
+//
+echo "Hola $nombre $apellido";
